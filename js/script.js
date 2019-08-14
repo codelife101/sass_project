00f5d52a283925aa5d967d6aec112809d8ba5ac6
Jake PRESTON <270024896@bun2-206-10.local>
2019-08-14 15:33:51 +1200

    gekgfjoq

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('testing 123');
+
+$(document).ready(function(){
+
+	$('.menu').click(function(){
+		$('nav').toggleClass('active');
+	})
+	// $('#close').click(function(){
+	// 	$('nav').hide();
+	// })
+
+
+ 
+
+});
